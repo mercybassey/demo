30d3f6e4521a1560b95fb0f1b386b92d86543fd8
M_ercy <mercybassey683@gmail.com>
2023-02-09 09:31:41 +0100

    first commit

---
+++ etcd.bash
@@ -0,0 +1,5 @@
+kubectl -n kube-system exec -it etcd-master -- sh -c "ETCDCTL_API=3 etcdctl \
+   --cacert=/etc/kubernetes/pki/etcd/ca.crt   \
+   --cert=/etc/kubernetes/pki/etcd/server.crt \
+   --key=/etc/kubernetes/pki/etcd/server.key  \
+   get /registry/secrets/default/demo"
